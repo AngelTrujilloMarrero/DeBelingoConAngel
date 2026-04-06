e31dca9d3e29b77c3f4629141e701f0b07b4349c
AngelTrujilloMarrero <atrujimar@gmail.com>
2026-04-06 20:38:36 +0100

    Fix CORS credentials conflict

--- api/_cors.js
+++ api/_cors.js
@@ -18,8 +18,7 @@ export function applySecurityHeaders(req, res) {
 
   // CORS (Permisivo para pruebas)
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
